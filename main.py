e85a0a8c2178f9c2f607694154edba843cac7367
AstridBrogren <astrid_brogren@biswashington.org>
2025-01-23 17:50:21 +0000

    added

--- main.py
+++ main.py
@@ -1,1 +1,71 @@
+score = 0
+question = "what is the capital of sweden?"
+print(question)
+
+user_answer = input("your answer: ")
+
+if user_answer.lower() == "paris":
+    print("correct!")
+else:
+    print("oops! the correct answer is stocholm.")
+
+score += 1
+
+print(f"your score is: {score}")
+
+question = "what is the president of the united states?"
+print(question)
+
+user_answer = input("your answer: ")
+
+if user_answer.lower() == "donald trump":
+    print("correct!")
+else:
+    print("oops! the correct answer is donald trump.")
+
+score += 1
+
+print(f"your score is: {score}")
+
+question = "who is the owner of tik tok?"
+print(question)
+
+user_answer = input("your answer: ")
+
+if user_answer.lower() == "bytedance":
+    print("correct!")
+else:
+    print("oops! the correct answer is bytedance.")
+
+score += 1
+
+print(f"your score is: {score}")
+
+question = "what is the worlds population?"
+print(question)
+
+user_answer = input("your answer: ")
+
+if user_answer.lower() == "8 billion":
+    print("correct!")
+else:
+    print("oops! the correct answer is * billion.")
+
+score += 1
+
+print(f"your score is: {score}")
+
+question = "how many colors in the rain bow?"
+print(question)
+
+user_answer = input("your answer: ")
+
+if user_answer.lower() == "7":
+    print("correct!")
+else:
+    print("oops! the correct answer is 7.")
+
+score += 1
+
+print(f"your score is: {score}")
 
